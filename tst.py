8050142a0ea8f13f4af006188704303822917989
ap5967ap <aditya.priyadarshi@iiitb.ac.in>
2024-04-20 04:43:20 +0530

    done_almost

---
+++ tst.py
@@ -0,0 +1,12 @@
+import subprocess
+count = 0
+tot=100
+for i in range(1, tot):
+    process = subprocess.Popen(['python3', 'a.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = process.communicate()
+    output = output.decode().strip()
+    num1, num2 = map(int, output.split())
+    if num1 == num2:
+        count += 1
+
+print(f'{count}/{tot}')
